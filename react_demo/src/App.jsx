cab2d5dee716260988794b71b42c5e3aca177bf5
pirates59 <lenishaghimire07@gmail.com>
2024-06-30 15:10:59 +0545

    new repo

---
+++ react_demo/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Login from "./pages/Login";
+import{BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App(){
+  return(
+<div className="App">
+<BrowserRouter>
+<Routes>
+<Route path ="/" elements={Login}/>
+</Routes>
+</BrowserRouter>
+
+
+</div>
+  );
+
+}
+export default App;
+
